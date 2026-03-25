20b16537ea560a54e452e4a62f13cb52b7d77d94
ry3 <rye.quip@protonmail.com>
2026-03-25 11:32:23 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import database as db
+
+app = Flask(__name__)
+
+with app.app_context():
+    db.initialize_database()
+
+@app.route("/")
+def main_page():
+    return render_template('main.html')
+
+@app.route("/practice")
+def practice_page():
+    return "You're in practice!"
+
+@app.route("/add")
+def add_page():
+    return "You're in add!"
+
+@app.route("/edit")
+def edit_page():
+    return "You're in edit!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+import os
+base_dir = os.path.dirname(os.path.abspath(__file__))
+words_database = os.path.join(base_dir, "words.db")
+
+def create_words_table(conn):
+    create_table = """
+        CREATE TABLE IF NOT EXISTS words (
+            id INTEGER PRIMARY KEY,
+            word TEXT NOT NULL,
+            struggled INTEGER DEFAULT 0
+        );
+    """
+    cursor = conn.cursor()
+    cursor.execute(create_table)
+
+def create_rhymes_table(conn):
+    create_table = """
+        CREATE TABLE IF NOT EXISTS rhymes (
+            id INTEGER PRIMARY KEY,
+            word_id INTEGER NOT NULL,
+            rhyme TEXT NOT NULL,
+            FOREIGN KEY (word_id) REFERENCES words(id)
+        );
+    """
+    cursor = conn.cursor()
+    cursor.execute(create_table)
+
+def initialize_database():
+    try:
+        with sqlite3.connect(words_database) as conn:
+            print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+            create_words_table(conn)
+            create_rhymes_table(conn)
+    except sqlite3.OperationalError as e:
+            print("Failed to open database:", e)
+
+if __name__ == "__main__":
+    initialize_database()
